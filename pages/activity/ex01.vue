5a65fd7b2dd154bfe7d77c320236f31f45d87887
porntip-it26 <ita5922@ccollege.ac.th>
2020-07-08 19:11:45 +0700

    ex

---
+++ pages/activity/ex01.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>Hello {{ name }}</h2>
+    <h2>Age {{ age }}</h2>
+    <h2> Name:<input type="text" :value="name"></h2>
+    <input type="text" v-model="name">
+    <div>
+      <button @click="doSave">
+        Save
+      </button>
+    </div>
+    <div>
+      <button @click="doMenu">
+        Main Menu
+      </button>
+    </div>
+    <div>
+      <button @click="doHome">
+        Home
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'Porntip Kunmee',
+      age: 19,
+    }
+  },
+  methods: {
+    doSave() {
+      console.log('Do Save')
+    },
+    doHome() {
+      this.$router.push('/')
+    },
+    doMenu() {
+      this.$router.replace('menu')
+    },
+  },
+}
+</script>
