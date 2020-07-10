c15fbb721a6c4df4b5ff2b73f18086c3b699a2a5
porntip-it26 <ita5922@ccollege.ac.th>
2020-07-10 14:59:40 +0700

    login_register

---
+++ pages/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+      <br>
+      <center>
+    <v-card width="450" color="#88DDBB">
+      <v-card-title-center>
+        <h1>Login</h1> 
+      </v-card-title-center>
+      <v-card-text>
+
+        <v-form>
+          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+
+          <v-text-field 
+            :type="showPassword ? 'text' : 'password'" 
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          />
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions>
+          <nuxt-link to="regis">
+        <v-btn class="ma-2" tile color="success" dark>REGISTER</v-btn>
+          </nuxt-link>
+        <v-spacer></v-spacer>
+        <v-btn class="ma-2" tile color="indigo" dark>LOGIN</v-btn>
+      </v-card-actions>
+
+    </v-card>
+    </center>
+  </v-app>
+</template>
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      showPassword: false
+    }
+  }
+}
+</script>
